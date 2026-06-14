49ecbe3750c6773b44b961c4fef787fd2f2eb436
Kesar Karale <kesarkarale1@gmail.com>
2026-06-14 20:19:00 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,32 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(
+  ...inputs: ClassValue[]
+) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(
+  date: Date
+) {
+  return new Intl.DateTimeFormat(
+    "en-IN",
+    {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    }
+  ).format(date);
+}
+
+export function truncateText(
+  text: string,
+  length = 40
+) {
+  if (text.length <= length) {
+    return text;
+  }
+
+  return text.slice(0, length) + "...";
 }
