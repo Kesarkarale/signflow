9141894753049894e4f7eb24d9ece5e958d52c02
Kesar Karale <kesarkarale1@gmail.com>
2026-06-14 21:03:43 +0530

    Create route.ts

---
+++ src/app/api/signatures/route.ts
@@ -0,0 +1,44 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const {
+      documentId,
+      imageUrl,
+    } = body;
+
+    const signature =
+      await prisma.signature.create({
+        data: {
+          documentId,
+          imageUrl,
+        },
+      });
+
+    await prisma.document.update({
+      where: {
+        id: documentId,
+      },
+      data: {
+        status: "SIGNED",
+      },
+    });
+
+    await prisma.auditLog.create({
+      data: {
+        action: "DOCUMENT_SIGNED",
+        documentId,
+      },
+    });
+
+    return NextResponse.json(signature);
+  } catch {
+    return NextResponse.json(
+      { error: "Failed" },
+      { status: 500 }
+    );
+  }
+}
