35746b50bd67b74dc0b26bfb435aa559c1575b27
Kesar Karale <kesarkarale1@gmail.com>
2026-06-14 21:23:34 +0530

    Create document-search.tsx

---
+++ src/components/document-search.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function DocumentSearch() {
+  return (
+    <input
+      placeholder="Search documents..."
+      className="
+      w-full
+      border
+      rounded-xl
+      px-4
+      py-3
+      "
+    />
+  );
+}
