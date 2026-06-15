7de807ad9fb038c9a2c533f060776f37a7f3c173
Kesar Karale <kesarkarale1@gmail.com>
2026-06-15 20:39:45 +0530

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,19 @@
-"use client";
+ "use client";
 
-import { SessionProvider } from "next-auth/react";
+import { ThemeProvider }
+from "next-themes";
 
 export default function Providers({
 children,
 }: {
 children: React.ReactNode;
 }) {
-return ( <SessionProvider>
-{children} </SessionProvider>
+return ( <ThemeProvider
+   attribute="class"
+   defaultTheme="dark"
+   enableSystem
+ >
+{children} </ThemeProvider>
 );
 }
+
