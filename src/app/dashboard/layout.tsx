dbfbf399c208a8735c31fb46734e79bd1c76bb43
Kesar Karale <kesarkarale1@gmail.com>
2026-06-16 19:58:55 +0530

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,14 +6,21 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex">
-
+    <div className="flex bg-slate-100 dark:bg-slate-950">
       <Sidebar />
 
-      <main className="flex-1 bg-slate-50 min-h-screen">
+      <main
+        className="
+          flex-1
+          min-h-screen
+          bg-slate-100
+          dark:bg-slate-950
+          text-slate-900
+          dark:text-white
+        "
+      >
         {children}
       </main>
-
     </div>
   );
 }
