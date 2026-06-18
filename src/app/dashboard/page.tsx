0e37d7b166ebec83a30176494e693192a37ff011
Kesar Karale <kesarkarale1@gmail.com>
2026-06-18 20:35:18 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,7 +12,6 @@ export default async function DashboardPage() {
   if (!session) {
     redirect("/login");
   }
-export default async function DashboardPage() {
  
  const totalDocs =
   await prisma.document.count();
